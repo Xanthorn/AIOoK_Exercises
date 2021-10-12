faba17f4455511959513b8535af5e23b2313d8d3
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-10-12 17:26:30 +0200

    Completed exercise 1.2 Added multiplication and division functions, linked these to buttons

--- Exercise_1/calculator.js
+++ Exercise_1/calculator.js
@@ -5,65 +5,88 @@ var result = document.getElementById("result");
 var firstNumber = document.getElementById("firstNumber");
 var secondNumber = document.getElementById("secondNumber");
 
-function firstCheck(){
-    if(isNaN(parseInt(firstNumber.value))){
-      result.textContent = "Only numbers !!";
-      this.value = "";
+function firstCheck() {
+    if (isNaN(parseInt(firstNumber.value))) {
+        result.textContent = "Only numbers !!";
+        this.value = "";
     } else {
-      result.textContent = "";
+        result.textContent = "";
     }
-  }
-  
-  function secondCheck(){
-    if(isNaN(parseInt(secondNumber.value))){
-      result.textContent = "Only numbers !!";
-      this.value = ""; 
-    } else {
-      result.textContent = "";
-    }
-  }
-
-function addition(){
-    let first = parseInt(firstNumber.value);
-    let second = parseInt(secondNumber.value);
-
-    if(!isNaN(first) && !isNaN(second)){
-    score.textContent = first + second;
-    mark.textContent = "+";
-    } else {
-      result.textContent = "Please put some numbers in the given fields!";}
 }
 
-function subtraction(){
+function secondCheck() {
+    if (isNaN(parseInt(secondNumber.value))) {
+        result.textContent = "Only numbers !!";
+        this.value = "";
+    } else {
+        result.textContent = "";
+    }
+}
+
+function addition() {
     let first = parseInt(firstNumber.value);
     let second = parseInt(secondNumber.value);
 
-    if(!isNaN(first) && !isNaN(second)){
-    score.textContent = first - second;
-    mark.textContent = "-";
+    if (!isNaN(first) && !isNaN(second)) {
+        score.textContent = first + second;
+        mark.textContent = "+";
     } else {
-      result.textContent = "Please put some numbers in the given fields!";
+        result.textContent = "Please put some numbers in the given fields!";
     }
- }
+}
 
- function exponentiation(){
+function subtraction() {
     let first = parseInt(firstNumber.value);
     let second = parseInt(secondNumber.value);
 
-    if(!isNaN(first) && !isNaN(second)){
-    let result = first;
-
-    if(second === 0){
-      result = 1;
-    }
-
-    for(let i = 1; i < second;i++){
-      result = result * first;
-    }
-
-    score.textContent = result;
-    mark.textContent = "^";
+    if (!isNaN(first) && !isNaN(second)) {
+        score.textContent = first - second;
+        mark.textContent = "-";
     } else {
-      result.textContent = "Please put some numbers in the given fields!";
+        result.textContent = "Please put some numbers in the given fields!";
+    }
+}
+
+function multiplication() {
+    let first = parseInt(firstNumber.value);
+    let second = parseInt(secondNumber.value);
+    if (!isNaN(first) && !isNaN(second)) {
+        score.textContent = first * second;
+        mark.textContent = "*";
+    } else {
+        result.textContent = "Please put some numbers in the given fields!";
+    }
+}
+
+function division() {
+    let first = parseInt(firstNumber.value);
+    let second = parseInt(secondNumber.value);
+    if (!isNaN(first) && !isNaN(second)) {
+        score.textContent = first / second;
+        mark.textContent = "/";
+    } else {
+        result.textContent = "Please put some numbers in the given fields!";
+    }
+}
+
+function exponentiation() {
+    let first = parseInt(firstNumber.value);
+    let second = parseInt(secondNumber.value);
+
+    if (!isNaN(first) && !isNaN(second)) {
+        let result = first;
+
+        if (second === 0) {
+            result = 1;
+        }
+
+        for (let i = 1; i < second; i++) {
+            result = result * first;
+        }
+
+        score.textContent = result;
+        mark.textContent = "^";
+    } else {
+        result.textContent = "Please put some numbers in the given fields!";
     }
 }
