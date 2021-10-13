87b36733a38767e39836dad9e2b32f8324edd357
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-10-13 21:37:38 +0200

    Added js file for distance calculating Added js file required for calculating distance between two points

--- Exercise_1/calculator.js
+++ Exercise_1/calculator.js
@@ -5,6 +5,22 @@ var result = document.getElementById("result");
 var firstNumber = document.getElementById("firstNumber");
 var secondNumber = document.getElementById("secondNumber");
 
+var distanceResult = document.getElementById("distanceResult");
+
+var firstPoint = {
+    x: document.getElementById("firstX"),
+    y: document.getElementById("firstY"),
+    typeOfX: document.querySelector('input[name="typeOfFirstX"]:checked'),
+    typeOfY: document.querySelector('input[name="typeOfFirstY"]:checked')
+};
+
+var secondPoint = {
+    x: document.getElementById("secondX"),
+    y: document.getElementById("secondY"),
+    typeOfX: document.querySelector('input[name="typeOfSecondX"]:checked'),
+    typeOfY: document.querySelector('input[name="typeOfSecondY"]:checked')
+};
+
 function firstCheck() {
     if (isNaN(parseInt(firstNumber.value))) {
         result.textContent = "Only numbers !!";
@@ -92,4 +108,52 @@ function exponentiation() {
     } else {
         result.textContent = "Please put some numbers in the given fields!";
     }
+}
+
+function validateLatitude(x) {
+    if (isNaN(parseInt(x.value))) {
+        result.textContent = "Only numbers !!";
+        this.value = "";
+    } else {
+        result.textContent = "";
+    }
+}
+
+function calculateDistance() {
+    let firstX = parseInt(firstPoint.x.value);
+    let firstY = parseInt(firstPoint.y.value);
+    let secondX = parseInt(secondPoint.x.value);
+    let secondY = parseInt(secondPoint.y.value);
+
+    if (firstPoint.typeOfX.value === "S") {
+        firstX *= -1;
+    }
+
+    if (firstPoint.typeOfY.value === "W") {
+        firstY *= -1;
+    }
+
+    if (secondPoint.typeOfX.value === "S") {
+        secondX *= -1;
+    }
+
+    if (secondPoint.typeOfY.value === "W") {
+        secondY *= -1;
+    }
+
+    let beforeSqrt = (secondX - firstX) * (secondX - firstX) + (secondY - firstY) * (secondY - firstY);
+    let output = Math.sqrt(beforeSqrt);
+    distanceResult.value = output;
+}
+
+function showDistanceCalculator() {
+    document.getElementById("standardInput").style.display = "none";
+    document.getElementById("actionMenu").style.display = "none";
+    document.getElementById("coordinatesInput").style.display = "block";
+}
+
+function hideDistanceCalculator() {
+    document.getElementById("standardInput").style.display = "block";
+    document.getElementById("actionMenu").style.display = "block";
+    document.getElementById("coordinatesInput").style.display = "none";
 }
