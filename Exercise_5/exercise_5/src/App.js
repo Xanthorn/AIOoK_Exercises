7b38963fce9c4cc4a6ab34b495b6264ad0a1da1d
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-15 15:40:18 +0100

    Add blank exercise 5 project

---
+++ Exercise_5/exercise_5/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import Products from './Products';
+
+function App() {
+  return (
+    <div className="container">
+      <div className="row">
+        <div className="col">
+          <Products />
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ Exercise_5/exercise_5/src/Products.jsx
@@ -0,0 +1,100 @@
+import React, { Component } from "react";
+import ProductClass, { statuses } from "./class/ProductClass";
+import Product from "./Product";
+import './Product.css';
+import AddProduct from "./AddProduct";
+import { ThermometerSnow } from "react-bootstrap-icons";
+
+class Products extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            products: [
+                new ProductClass("Masło", statuses.ACTIVE),
+                new ProductClass("Chleb", statuses.INBASKET),
+                new ProductClass("Ciastka", statuses.BOUGHT)
+            ]
+        }
+    }
+
+    addProduct = (s) => {
+        this.setState(state => {
+            let productsList = state.products;
+
+            let newProduct = new ProductClass(s.name, s.status);
+            productsList.push(newProduct);
+
+            return { products: productsList }
+        });
+    }
+
+    setProductStatus = (id, status) => {
+        this.setState(state => {
+            let productsList = state.products;
+
+            for (let i = 0; i < productsList.length; i++) {
+                if (productsList[i].id === id) {
+                    productsList[i].status = status;
+                }
+            }
+
+            return { products: productsList }
+        });
+    }
+
+    deleteProduct = (id) => {
+        this.setState(state => {
+            let productsList = state.products;
+
+            productsList = productsList.filter(x => {
+                return x.id != id;
+            });
+
+            return { products: productsList }
+        })
+    }
+
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <table className="table table-hover">
+                            <thead>
+                                <tr>
+                                    <th className="text-center">Nazwa</th>
+                                    <th className="text-center">Status</th>
+                                    <th className="text-center">Zarządzanie</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    this.state.products.map((product, key) => {
+                                        return (
+                                            <Product
+                                                key={key}
+                                                id={product.id}
+                                                name={product.name}
+                                                status={product.status}
+                                                setProductStatus={this.setProductStatus}
+                                                deleteProduct={this.deleteProduct}
+                                            />
+                                        );
+                                    })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+
+                <div className="row">
+                    <div className="col">
+                        <AddProduct addProduct={this.addProduct} />
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Products;
