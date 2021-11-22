bf213bbf5256554202a8e17e86de68238fffa902
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-22 13:16:47 +0100

    Add exercise 6 api and db projects

---
+++ Exercise_6/exercise-6-api/exercise-6-db/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace exercise_6_db.Models
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Status Status { get; set; } = Status.Active;
+    }
+
+    public enum Status
+    {
+        Active,
+        InBasket,
+        Bought
+    }
+}
