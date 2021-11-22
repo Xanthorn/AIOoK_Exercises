f117fa047d072d59df2db326fc6f3f85f040ca74
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-22 13:16:47 +0100

    Add exercise 6 api and db projects

---
+++ Exercise_6/exercise-6-api/exercise-6-db/DataContext.cs
@@ -0,0 +1,12 @@
+﻿using exercise_6_db.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace exercise_6_db
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
